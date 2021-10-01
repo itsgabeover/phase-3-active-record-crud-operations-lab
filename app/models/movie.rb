a18eea88cb7a68cd6965691bcd10ddbab58b417b
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-10-01 12:37:33 -0700

    commit tree 958a5e7c89917750dd61db135a921f00ab5333bd

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,25 @@
 class Movie < ActiveRecord::Base
+    
+  def self.create_with_title(title)
+    Movie.create(title: title)
+  end
+
+  def self.first_movie
+    Movie.first
+  end
+  
+  def self.last_movie
+    Movie.last
+  end
+
+  def self.movie_with_id(id)
+    Movie.find(id)
+  end
+
+  def self.movie_count
+    let movie_count = 0
+    Movie.all.map { |movie| movie_count = movie_count + 1}
+    movie_count
+  end
 
 end
